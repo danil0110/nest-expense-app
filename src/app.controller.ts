17905f18d63fe258d3e00de07884a16467ceff1f
Danil Zalizchuk <danilmail0110@gmail.com>
2022-10-28 16:42:11 +0300

    create, update, delete report

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
 import { data, ReportType } from './data';
 
 @Controller('report/:type')
@@ -21,17 +32,60 @@ export class AppController {
   }
 
   @Post()
-  createReport() {
-    return 'created';
+  createReport(
+    @Body() { source, amount }: { source: string; amount: number },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+
+    data.reports.push(newReport);
+    return newReport;
   }
 
   @Put(':id')
-  updateReport() {
-    return 'updated';
+  updateReport(
+    @Body() body: { source: string; amount: number },
+    @Param('type') type: string,
+    @Param('id') id: string,
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportToUpdateIdx = data.reports.findIndex(
+      (r) => r.type === reportType && r.id === id,
+    );
+
+    if (reportToUpdateIdx < 0) return;
+
+    data.reports[reportToUpdateIdx] = {
+      ...data.reports[reportToUpdateIdx],
+      ...body,
+    };
+
+    return data.reports[reportToUpdateIdx];
   }
 
   @Delete(':id')
-  deleteReport() {
-    return 'deleted';
+  @HttpCode(204)
+  deleteReport(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportToDelete = data.reports.find(
+      (r) => r.type === reportType && r.id === id,
+    );
+
+    if (!reportToDelete) return;
+
+    data.reports = data.reports.filter(
+      (r) => r.type === reportType && r.id !== id,
+    );
+
+    return reportToDelete;
   }
 }
