7f25d961f3e2e1d28ccb1e32b57462c76f7f2614
Danil Zalizchuk <danilmail0110@gmail.com>
2022-10-28 15:54:37 +0300

    get report by id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,8 +11,13 @@ export class AppController {
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.reports.find(
+      (report) => report.type === reportType && report.id === id,
+    );
   }
 
   @Post()
